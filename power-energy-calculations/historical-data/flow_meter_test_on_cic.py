f5fa2e0c09acadbd0e4926a9abaac3923542cbe5
noutn <noutnmr@gmail.com>
2023-05-31 13:37:48 +0200

    changes in flow_meter_test python script

--- power-energy-calculations/historical-data/flow_meter_test_on_cic.py
+++ power-energy-calculations/historical-data/flow_meter_test_on_cic.py
@@ -8,19 +8,27 @@ import numpy as np
 import time
 import pandas as pd
 
+# define redis adresses
+CONTROLLER_FLAG_ADRESS = '9008'
+INTERNAL_FLOW_METER_ADRESS = '31'
+EXTERNAL_FLOW_METER_ADRESS = '30030'
+PUMP_RELAY_ADRESS = '103'
+PUMP_DUTY_CYCLE_ADRESS = '104'
+
+
 def main():
 
-    df = pd.DataFrame(columns=['pump_duty_cycle', 'internal_flow', 'external_flow'])
+    df = pd.DataFrame(columns=['time', 'pump_duty_cycle', 'internal_flow', 'external_flow'])
 
 
     redis = QuattRedis()
     redis.connect()
     
     # set controller flag alive
-    redis.mset({'9008':1})
+    redis.mset({CONTROLLER_FLAG_ADRESS:1})
 
     # switch on pump relay
-    redis.mset({'103':1})
+    redis.mset({PUMP_RELAY_ADRESS:1})
 
     # set pump duty cycles
     pump_duty_cycles = [100, 250, 400, 550, 700, 850]
@@ -28,24 +36,25 @@ def main():
     for pump_duty_cycle in pump_duty_cycles:
         
         # set pump duty cycle
-        redis.mset({'104':pump_duty_cycle})
+        redis.mset({PUMP_DUTY_CYCLE_ADRESS:pump_duty_cycle})
         time.sleep(30) # wait 30 seconds to reach steady state
 
         # take measurements
         for _ in range(10):
             # take measurements
-            internal_measurement = redis.get('31')
-            external_measurement = redis.get('32') # still needs to be set up
+            internal_measurement = redis.get(INTERNAL_FLOW_METER_ADRESS)
+            external_measurement = redis.get(EXTERNAL_FLOW_METER_ADRESS) # still needs to be set up
 
             #add measurements to pandas frame
-            df = df.append({'pump_duty_cycle':pump_duty_cycle, 
+            df = df.append({'time':time.time(),
+                            'pump_duty_cycle':pump_duty_cycle, 
                             'internal_flow':internal_measurement, 
                             'external_flow':external_measurement}, ignore_index=True)
             
             time.sleep(5) # wait 5 seconds between measurements
     
     # save dataframe to csv
-    df.to_csv(f'flow_meter_test_{int(time.time())}.csv', index=False)
+    df.to_csv(f'data/flow_meter_test_{int(time.time())}.csv', index=False)
 
 
 if __name__=='__main__':
